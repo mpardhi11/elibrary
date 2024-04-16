dc3debfe8f33f882d28563d32e64256a691d08d1
Mohit Pardhi <mpardhi11@gmail.com>
2024-04-16 09:46:43 +0530

    User database model created.

---
+++ src/users/userModel.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import { User as UserInterface } from './userTypes';
+
+const userSchema = new mongoose.Schema<UserInterface>(
+  {
+    name: { type: String, required: [true, 'Name is required'] },
+
+    email: { type: String, required: [true, 'Email is required'], unique: true },
+
+    password: { type: String, required: [true, 'Password is required'] },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Collection name is 'User'
+export const User = mongoose.model('User', userSchema);
