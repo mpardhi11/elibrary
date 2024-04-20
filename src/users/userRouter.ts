fab3f5fb2abe65c5ee9ca5bd1249636e4310d771
Mohit Pardhi <34490195+mpardhi11@users.noreply.github.com>
2024-04-20 15:58:53 +0530

    Add loginUser route to userRouter.ts

--- src/users/userRouter.ts
+++ src/users/userRouter.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express';
-import { createUser } from './userController';
+import { createUser, loginUser } from './userController';
 
 const userRouter = express.Router();
 
 // POST /register
 userRouter.post('/register', createUser);
+userRouter.post('/login', loginUser)
 
 export default userRouter;
