db646015c58c8aa19366eef4a68e1b270fc894af
Mohit Pardhi <34490195+mpardhi11@users.noreply.github.com>
2024-04-21 13:28:04 +0530

    Refactor file upload functionality in bookRouter.ts

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -2,21 +2,23 @@ import express, { Request, Response } from 'express';
 import { createBook } from './bookController';
 import multer from 'multer';
 import path from 'node:path';
+import { authenticate } from '../middlewares/authenticate';
 
 const bookRouter = express.Router();
 
+// Setting up multer for file upload
 const upload = multer({
-    dest: path.join(__dirname, '../../public/data/uploads'),
-    limits: { fileSize: 9.5e6 }, // 9.5MB limit
-})
-/**
- * Method : POST
- * Route : api/books
- * Description : Create a new book
- *  */
-bookRouter.post('/', upload.fields([
-    { name: 'coverImage', maxCount: 1 },
-    { name: 'file', maxCount: 1 }
+    dest: path.join(__dirname, '../../public/data/uploads'), // Destination folder for uploaded files
+    limits: { fileSize: 9.5e6 }, // 9.5MB limit for file size
+});
+
+
+// POST endpoint for creating a new book
+// Path: /api/books
+// Access: Private
+bookRouter.post('/', authenticate, upload.fields([
+    { name: 'coverImage', maxCount: 1 }, // Field for cover image upload (max 1 file)
+    { name: 'file', maxCount: 1 } // Field for book file upload (max 1 file)
 ]), createBook);
 
 export default bookRouter;
