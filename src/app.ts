a21fe6ec9d464db715fa3f36eba7ec4797a1c29a
Mohit Pardhi <34490195+mpardhi11@users.noreply.github.com>
2024-04-21 18:19:49 +0530

    Add CORS middleware and set custom 'X-Powered-By' header value in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,22 @@ import express, { Request, Response } from 'express';
 import { errorHandler } from './middlewares/globalErrorHandler';
 import userRouter from './users/userRouter';
 import bookRouter from './book/bookRouter';
+import cors from 'cors';
+import { config } from './config/config';
 
 const app = express();
 
+// Disable the default 'X-Powered-By' header
+app.disable('x-powered-by');
+
+// Set your own value for the 'X-Powered-By' header
+app.use((req, res, next) => {
+  res.set('X-Powered-By', config.XPoweredBy);
+  next();
+});
+
+// Middleware to enable CORS
+app.use(cors({ origin: config.frontDomain, credentials: true, }))
 // Middleware to parse incoming requests
 app.use(express.json());
 // Middleware to parse incoming requests with urlencoded payloads
@@ -12,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to serve static files from the 'public' folder
 app.use(express.static('public'));
 
+// Route handler for the test endpoint
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello, Express!' });
 });
