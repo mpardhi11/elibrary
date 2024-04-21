ad5dd1317b66ccd430983ecf9296e3e9cebb2bb7
Mohit Pardhi <34490195+mpardhi11@users.noreply.github.com>
2024-04-21 18:20:01 +0530

    Update config.ts to add frontDomain and XPoweredBy environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -13,7 +13,9 @@ const _config = {
     cloudName: process.env.CLOUDINARY_CLOUD_NAME || '',
     apiKey: process.env.CLOUDINARY_API_KEY || '',
     apiSecret: process.env.CLOUDINARY_API_SECRET || '',
-  }
+  },
+  frontDomain: process.env.FRONT_DOMAIN || 'http://localhost:3000',
+  XPoweredBy: process.env.X_POWERED_BY || '',
 };
 
 // Freeze the config object to prevent further changes
